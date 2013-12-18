1479917084a7ada3ee8056a5a96a11e9f355cd32
Zongmin Lei <leizongmin@gmail.com>
2013-12-18 18:23:01 +0800

    php处理

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
+/*
 var http = require('http');
+
 var CGI = require('./lib/cgi');
 
 var cgi = new CGI('php-cgi', {
@@ -9,3 +11,20 @@ var s = http.createServer(function (req, res) {
   cgi.handle(req, res);
 });
 s.listen(3000);
+*/
+
+var connect = require('connect');
+var php = require('./lib/php');
+
+var app = connect();
+app.use(php({
+  process: {
+    limit:   1,
+    //timeout: 1000
+  },
+  queue: {
+    limit:   1,
+    //timeout: 2000
+  }
+}));
+app.listen(3000);
