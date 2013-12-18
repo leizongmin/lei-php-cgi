33181872d180e410dc003c241083a6708b453570
Zongmin Lei <leizongmin@gmail.com>
2013-12-18 15:55:59 +0800

    cgi可指定启动参数

--- test.js
+++ test.js
@@ -3,7 +3,8 @@ var CGI = require('./lib/cgi');
 
 var cgi = new CGI('php-cgi', {
   DOCUMENT_ROOT: __dirname
-});
+}, ['-c', __dirname]);
+console.log(cgi);
 var s = http.createServer(function (req, res) {
   cgi.handle(req, res);
 });
